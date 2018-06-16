4cc77b5058ad8a58b8cee8c3cef3c5d525694c6c
Janice Darikho <janicedarikho@gmail.com>
2018-06-16 06:54:49 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,16 +8,16 @@ module Players
         move = [1, 3, 7, 9].detect { |i|
           !board.taken?(i)
         }.to_s
-      elsif board.turn_count == 2 && board.taken?(1) && !board.taken?(9)
+      elsif board.turn_count == 2 && !board.taken?(1) && !board.taken?(9)
         move = "9"
       elsif board.turn_count == 2 && board.taken?(3) && !board.taken?(7)
         move = "7"
       elsif board.turn_count == 2 && board.taken?(7) && !board.taken?(3)
         move = "3"
       elsif board.turn_count == 2 && board.taken?(9) && !board.taken?(1)
-        move = "1"  
-      elsif 
-        
+        move = "1"
+      elsif
+
       end
 
     end
