6a6230f7482a20aa295c65df0c163d40537b367d
Janice Darikho <janicedarikho@gmail.com>
2018-06-16 06:36:33 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,17 +4,10 @@ module Players
     def move(board)
       if !board.taken?(5)
         move = "5"
-      elsif board.taken?(5) && !board.taken?(1)
-        move = "1"
-      elsif board.taken?(5) && board.taken?(1) && !board.taken?(3)
-        move = "3"
-      elsif board.taken?(5) && board.taken?(1) && board.taken?(3) && !board.taken?(7)
-        move = "7"
       else
-        move = "9"
+        [1, 3, 7, 9].sample.to_s
       end
 
-      
     end
 
   end
