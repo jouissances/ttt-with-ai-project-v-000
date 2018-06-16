058f2b6d122d62b4236a066b15c7d5ad23ec173e
Janice Darikho <janicedarikho@gmail.com>
2018-06-16 07:22:55 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -12,7 +12,7 @@ module Players
 
       # If you went first (and took the middle), take a corner square with your second move.
       elsif board.turn_count == 2
-        move = [1, 3, 7, 9].detect { |i| 
+        move = [1, 3, 7, 9].detect { |i|
           !board.taken?(i)
         }.to_s
 
@@ -22,35 +22,35 @@ module Players
 
       # From here on, run through the WIN_COMBINATIONS array, checking whether any of the combinations have two squares filled with the same token and a third, empty square.
       else
-        Game::WIN_COMBINATIONS.detect do |combo|
+        Game::WIN_COMBINATIONS.detect { |combo|
 
           # First, check whether you have any chances to win, since it doesn't matter whether the opponent has a chance to win if you can win first.
-          if combo.select { |i| 
+          if combo.select { |i|
             board.position(i + 1) == token
-          }.size == 2 && combo.any? { |i| 
+          }.size == 2 && combo.any? { |i|
             board.position(i + 1) == " "
           }
 
-            move = combo.select { |i| 
+            move = combo.select { |i|
               !board.taken?(i + 1)
             }.first.to_i.+(1).to_s
 
           # If you can't play any winning moves, play a move to block the opponent from winning.
-          elsif combo.select { |i| 
+          elsif combo.select { |i|
             board.position(i + 1) != " " && board.position(i+1) != token
-          }.size == 2 && combo.any? { |i| 
+          }.size == 2 && combo.any? { |i|
             board.position(i + 1) == " "
           }
 
-            move = combo.select { |i| 
+            move = combo.select { |i|
               !board.taken?(i + 1)
             }.first.to_i.+(1).to_s
 
           end
-        end
+        }
 
         # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
-        move = [1, 3, 7, 9, 2, 4, 6, 8].detect { |i| 
+        move = [1, 3, 7, 9, 2, 4, 6, 8].detect { |i|
           !board.taken?(i)
         }.to_s if move == nil
       end
