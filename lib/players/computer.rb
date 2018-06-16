3924bdf62fe96647e27f4d115812201a010428fb
Janice Darikho <janicedarikho@gmail.com>
2018-06-16 06:37:53 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,7 +5,7 @@ module Players
       if !board.taken?(5)
         move = "5"
       else
-        [1, 3, 7, 9].detect { |i|
+        move = [1, 3, 7, 9].detect { |i|
           !board.taken(i)
         }.to_s
       end
