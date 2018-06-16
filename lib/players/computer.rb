2ab4a623b1c5ed03098e4d16b3493f1609a0928a
Janice Darikho <janicedarikho@gmail.com>
2018-06-16 06:33:35 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,9 +2,17 @@ module Players
   class Computer < Player
 
     def move(board)
-      board.cells.each_index.select { |i|
-        board.cells[i] == " "
-      }.sample
+      if !board.taken?(5) 
+        move = "5" 
+      elsif board.taken?(5) && !board.taken?(1)
+        move = "1"
+      elsif board.taken?(5) && board.taken?(1) && !board.taken?(3)
+        move = "3"
+      elsif board.taken?(5) && board.taken?(1) && board.taken?(3) && !board.taken?(7)
+        move = "7"
+      else
+        move = "9"
+      end
     end
 
   end
