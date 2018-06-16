5194525c0cb14c71c1943105b7e26b80bbccdc0a
Janice Darikho <janicedarikho@gmail.com>
2018-06-16 07:04:00 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,19 +2,19 @@ module Players
   class Computer < Player
 
     def move(board)
-      if board.turn_count == 1 && !board.taken?(5)
+      if !board.taken?(5)
         move = "5"
-      elsif board.turn_count == 1 && board.taken?(5)
+      elsif board.taken?(5)
         move = [1, 3, 7, 9].detect { |i|
           !board.taken?(i)
         }.to_s
-      elsif board.turn_count == 3 && !board.taken?(1) && !board.taken?(9)
+      elsif !board.taken?(1) && !board.taken?(9)
         move = "9"
-      elsif board.turn_count == 3 && board.taken?(3) && !board.taken?(7)
+      elsif board.taken?(3) && !board.taken?(7)
         move = "7"
-      elsif board.turn_count == 3 && board.taken?(7) && !board.taken?(3)
+      elsif board.taken?(7) && !board.taken?(3)
         move = "3"
-      elsif board.turn_count == 3 && board.taken?(9) && !board.taken?(1)
+      elsif board.taken?(9) && !board.taken?(1)
         move = "1"
       elsif board.turn_count == 5
         move = [1, 3, 7, 9].detect { |i|
